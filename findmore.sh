f19cefa9b536720384e1bf877ef1bb7bd3012acf
David Timoteo Dimande <davidtimoteodimande@gmail.com>
2023-08-23 02:56:39 +0300

    find all files with 777 permission mode

--- findmore.sh
+++ findmore.sh
@@ -6,3 +6,7 @@
 # find all files owned by root user located in / dir
 
 find / -user root -type f 2> /dev/null
+
+# find all files owned by root user with 777 permission mode
+
+find / -user root -perm 777
