d11c5df61b53f79a4d78e30af0abfdc2e21554d2
David Timoteo Dimande <davidtimoteodimande@gmail.com>
2023-08-24 00:02:40 +0300

    organized in function and accept user input

--- findmore.sh
+++ findmore.sh
@@ -3,14 +3,27 @@
 # Author: David Dimande
 # Version 0.1
 
+findByUser () {
 # find all files owned by root user located in / dir
+echo "Enter the username to find files"
+read username
+find / -user $username -type f 2> /dev/null
+}
 
-find / -user root -type f 2> /dev/null
-
+findByPerm () {
 # find all files owned by root user with 777 permission mode
+echo "Enter username to find files"
+read username
+echo "Enter permission mode using octal base"
+read permission
+find / -user $username -perm $permission 2> /dev/null
+}
 
-find / -user root -perm 777
-
+findByUID () {
 # find files with specif UID and permission mode
-
-find / -uid 1000 -perm 777
+echo "Enter  the UID to find files"
+read uid
+echo "Enter permission mode using octal base"
+read permission
+find / -uid $uid -perm $permission 2> /dev/null
+}
