0dba0b2dbe91c10219c2808c235fe564c646b25a
David Timoteo Dimande <davidtimoteodimande@gmail.com>
2023-08-24 00:28:25 +0300

    added text menu and case statement

--- findmore.sh
+++ findmore.sh
@@ -27,3 +27,29 @@ echo "Enter permission mode using octal base"
 read permission
 find / -uid $uid -perm $permission 2> /dev/null
 }
+
+menu () {
+clear
+echo
+echo -e "\t\t\Welcome to findmore menu\n"
+echo -e "\t1. find files using username"
+echo -e "\t2. find files using username and permission mode"
+echo -e "\t3. find files using UID and permission mode"
+echo -e "\t0. Exit menu"
+echo -e "\t\tEnter option"
+read -n 1 option
+}
+menu
+case $option in
+0)
+    break ;;
+1)
+    findByUser ;;
+2)
+    findByPerm ;;
+3)
+    findByUID ;;
+*)
+clear
+echo "sorry you selected wrong option" ;;
+esac
