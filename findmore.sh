eccd574a7bba85a21b1eb93c751d1fbf1e437906
David Timoteo Dimande <davidtimoteodimande@gmail.com>
2023-08-23 03:02:32 +0300

    find files with specif UID and permission mode

--- findmore.sh
+++ findmore.sh
@@ -10,3 +10,7 @@ find / -user root -type f 2> /dev/null
 # find all files owned by root user with 777 permission mode
 
 find / -user root -perm 777
+
+# find files with specif UID and permission mode
+
+find / -uid 1000 -perm 777
